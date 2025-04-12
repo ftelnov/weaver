391df0974042d28f88d2f8c347fe1f5640e8fb0d
Fedor Telnov <ftelnov@protonmail.com>
2025-04-12 18:16:42 +0300

    fix macos ci install script

--- helpers/ci_macos_install.sh
+++ helpers/ci_macos_install.sh
@@ -2,7 +2,16 @@
 set -e
 
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- --default-toolchain 1.85 -y
+
 brew tap picodata/homebrew-tap
 brew install tarantool-picodata picodata
+
+# Extract icu4c 73 to replace newer version with it.
+# Needed by tarantool
+brew extract --version=73.2 icu4c $PWD/local-tap
+brew install icu4c@73.2
+rm -rf /opt/homebrew/opt/icu4c
+ln -s /opt/homebrew/opt/icu4c@73.2 /opt/homebrew/opt/icu4c
+
 cargo install --features="bin" tarantool-test --version "^0.3"
 cargo install tarantool-runner --version "^0.1.0"
