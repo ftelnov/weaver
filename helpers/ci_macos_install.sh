6db80c29d188d53d674e4a2d548a586f5a92d074
Fedor Telnov <ftelnov@protonmail.com>
2025-06-13 18:59:46 +0300

    fix ci

--- helpers/ci_macos_install.sh
+++ helpers/ci_macos_install.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 set -e
 
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- --default-toolchain 1.85 -y
-rustup component add rustfmt clippy
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- --default-toolchain 1.85 --profile default -y
 
 brew tap picodata/homebrew-tap
 brew install tarantool-picodata picodata
