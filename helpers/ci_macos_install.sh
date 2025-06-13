eac0492da308d212ad7f2523dab834d124625d20
Fedor Telnov <ftelnov@protonmail.com>
2025-06-13 18:56:16 +0300

    fix ci

--- helpers/ci_macos_install.sh
+++ helpers/ci_macos_install.sh
@@ -2,15 +2,15 @@
 set -e
 
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- --default-toolchain 1.85 -y
-rustup component add rustfmt
+rustup component add rustfmt clippy
 
 brew tap picodata/homebrew-tap
 brew install tarantool-picodata picodata
 
 # Extract icu4c 73 to replace newer version with it.
 # Needed by tarantool
-brew tap-new $USER/local-tap
-brew extract --version=73.2 icu4c $USER/local-tap
+brew tap-new "$USER"/local-tap
+brew extract --version=73.2 icu4c "$USER"/local-tap
 brew install icu4c@73.2
 
 rm -rf /opt/homebrew/opt/icu4c
